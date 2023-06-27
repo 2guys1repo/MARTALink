de8a2d82758fa76d65183719bb2303ec61af4636
Tuan Cai <tcai44@gatech.edum>
2023-06-27 12:03:40 -0400

    payment and user api

--- server/mongodb/index.js
+++ server/mongodb/index.js
@@ -1,20 +1,20 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-    if (mongoose.connections[0].readyState) return;
+  if (mongoose.connections[0].readyState) return;
 
-    await mongoose
-        .connect(process.env.MONGO_DB, {
-            dbName: process.env.DB_NAME,
-        })
-        .catch((e) => {
-            console.error("Error connecting to database.");
-            throw e;
-        });
-    console.log("Sucessfully connected to the database!");
-};
+  await mongoose
+    .connect(process.env.MONGO_DB, {
+      dbName: process.env.DB_NAME,
+    })
+    .catch((e) => {
+      console.error("Error connecting to database.");
+      throw e;
+    });
+  console.log("Sucessfully connected to the database!");
+}
 
 export async function closeDB() {
-    await mongoose.connection.close();
-    console.log("Connection Closed.");
-};
+  await mongoose.connection.close();
+  console.log("Connection Closed.");
+}
